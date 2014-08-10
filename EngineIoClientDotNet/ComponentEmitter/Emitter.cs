5bc64def3ef47ba5b8b41a910a0a801468c16a01
MatthiasL <matthias_ludwig@yahoo.com>
2014-08-11 07:20:33 +1000

    Emitter started

--- EngineIoClientDotNet/ComponentEmitter/Emitter.cs
+++ EngineIoClientDotNet/ComponentEmitter/Emitter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,32 @@ namespace Quobject.EngineIoClientDotNet.ComponentEmitter
     /// </remarks>
     public class Emitter
     {
+        private ConcurrentDictionary<string, ConcurrentQueue<IListener>>  callbacks = new ConcurrentDictionary<string, ConcurrentQueue<IListener>>();
 
+        private ConcurrentDictionary<IListener, IListener> _onceCallbacks = new ConcurrentDictionary<IListener, IListener>();
+
+        /// <summary>
+        ///  Listens on the event.
+        /// </summary>
+        /// <param name="event1">event name</param>
+        /// <param name="fn"></param>
+        /// <returns>a reference to this object</returns>
+        public Emitter On(string event1, IListener fn)
+        {
+            ConcurrentQueue<IListener> callbacksLocal = this.callbacks[event1];
+            if (callbacksLocal == null)
+            {
+                callbacksLocal = new ConcurrentQueue<IListener>();
+                this.callbacks[event1] = callbacksLocal;
+            }
+            callbacksLocal.Enqueue(fn);
+            return this;
+        }
+
+
+    }
+
+    public interface IListener {
+        void Call(params object[] args);
     }
 }
