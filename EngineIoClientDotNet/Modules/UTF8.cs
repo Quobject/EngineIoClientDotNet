3f40d5291d73af6b303ef11ae126247b4af6c0d7
MatthiasL <matthias_ludwig@yahoo.com>
2014-08-10 12:06:46 +1000

    UTF8 started

---
+++ EngineIoClientDotNet/Modules/UTF8.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quobject.EngineIoClientDotNet.Modules
+{
+    public class UTF8
+    {
+        private static int[] byteArray;
+        private static int byteCount;
+        private static int byteIndex;
+
+        public static string Encode(string str)
+        {
+            
+        }
+
+    }
+}
