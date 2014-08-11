cfb8b1412e9e2672348cb80d11a57377820f7580
MatthiasL <matthias_ludwig@yahoo.com>
2014-08-12 07:17:55 +1000

    EmitterTests first test done

--- EngineIoClientDotNet/ComponentEmitter/Emitter.cs
+++ EngineIoClientDotNet/ComponentEmitter/Emitter.cs
@@ -16,31 +16,139 @@ namespace Quobject.EngineIoClientDotNet.ComponentEmitter
     /// </remarks>
     public class Emitter
     {
-        private ConcurrentDictionary<string, ConcurrentQueue<IListener>>  callbacks = new ConcurrentDictionary<string, ConcurrentQueue<IListener>>();
+        private ConcurrentDictionary<string, List<IListener>> callbacks = new ConcurrentDictionary<string, List<IListener>>();
 
         private ConcurrentDictionary<IListener, IListener> _onceCallbacks = new ConcurrentDictionary<IListener, IListener>();
 
         /// <summary>
+        /// Executes each of listeners with the given args.
+        /// </summary>
+        /// <param name="eventString">an event name.</param>
+        /// <param name="args"></param>
+        /// <returns>a reference to this object.</returns>
+        public Emitter Emit(string eventString, params object[] args)
+        {
+            if (this.callbacks.ContainsKey(eventString))
+            {
+                List<IListener> callbacksLocal = this.callbacks[eventString];
+                callbacksLocal = new List<IListener>(callbacksLocal);
+                foreach (var fn in callbacksLocal)
+                {
+                    fn.Call(args);
+                }                    
+            }
+            return this;            
+        }
+
+        /// <summary>
         ///  Listens on the event.
         /// </summary>
-        /// <param name="event1">event name</param>
+        /// <param name="eventString">event name</param>
         /// <param name="fn"></param>
         /// <returns>a reference to this object</returns>
-        public Emitter On(string event1, IListener fn)
+        public Emitter On(string eventString, IListener fn)
         {
-            ConcurrentQueue<IListener> callbacksLocal = this.callbacks[event1];
-            if (callbacksLocal == null)
+            if (!this.callbacks.ContainsKey(eventString))
             {
-                callbacksLocal = new ConcurrentQueue<IListener>();
-                this.callbacks[event1] = callbacksLocal;
+                this.callbacks[eventString] = new List<IListener>();            
             }
-            callbacksLocal.Enqueue(fn);
+            List<IListener> callbacksLocal = this.callbacks[eventString];
+            callbacksLocal.Add(fn);
             return this;
         }
 
-        public Emitter Once(string event1, IListener fn)
+        /// <summary>
+        /// Adds a one time listener for the event.
+        /// </summary>
+        /// <param name="eventString">an event name.</param>
+        /// <param name="fn"></param>
+        /// <returns>a reference to this object</returns>
+        public Emitter Once(string eventString, IListener fn)
         {
-            IListener on = 
+            var on = new OnceListener(eventString, fn, this);
+
+            _onceCallbacks.TryAdd(fn, on);
+            return this;
+
+        }
+
+        /// <summary>
+        /// Removes all registered listeners.
+        /// </summary>
+        /// <returns>a reference to this object.</returns>
+        public Emitter Off()
+        {
+            this.callbacks.Clear();
+            this._onceCallbacks.Clear();
+            return this;
+        }
+
+        /// <summary>
+        /// Removes all listeners of the specified event.
+        /// </summary>
+        /// <param name="eventString">an event name</param>
+        /// <returns>a reference to this object.</returns>
+        public Emitter Off(string eventString)
+        {
+
+            List<IListener> retrievedValue;
+            if (! this.callbacks.TryRemove(eventString, out retrievedValue))
+            {
+                Console.WriteLine("Emitter.Off Could not remove {0}",eventString);
+            }
+            if (retrievedValue != null)
+            {
+                foreach (var listener in retrievedValue)
+                {
+                    IListener notUsed;
+                    this._onceCallbacks.TryRemove(listener, out notUsed);
+                }
+            }           
+            return this;
+        }
+
+
+        /// <summary>
+        /// Removes the listener
+        /// </summary>
+        /// <param name="eventString">an event name</param>
+        /// <param name="fn"></param>
+        /// <returns>a reference to this object.</returns>
+        public Emitter Off(string eventString, IListener fn)
+        {
+            if (this.callbacks.ContainsKey(eventString))
+            {
+                List<IListener> callbacksLocal = this.callbacks[eventString];
+                IListener offListener;
+                this._onceCallbacks.TryRemove(fn, out offListener);
+                callbacksLocal.Remove(offListener ?? fn);                
+            }
+            return this;
+        }
+
+        /// <summary>
+        ///  Returns a list of listeners for the specified event.
+        /// </summary>
+        /// <param name="eventString">an event name.</param>
+        /// <returns>a reference to this object</returns>
+        public List<IListener> Listeners(string eventString)
+        {
+            if (this.callbacks.ContainsKey(eventString))
+            {
+                List<IListener> callbacksLocal = this.callbacks[eventString];
+                return callbacksLocal != null ? new List<IListener>(callbacksLocal) : new List<IListener>();
+            }
+            return new List<IListener>();
+        }
+
+        /// <summary>
+        /// Check if this emitter has listeners for the specified event.
+        /// </summary>
+        /// <param name="eventString">an event name</param>
+        /// <returns>bool</returns>
+        public bool HasListeners(string eventString)
+        {
+            return this.Listeners(eventString).Count > 0;
         }
 
     }
@@ -49,12 +157,25 @@ namespace Quobject.EngineIoClientDotNet.ComponentEmitter
         void Call(params object[] args);
     }
 
-    public class Listener : IListener
+
+
+    public class OnceListener : IListener
     {
+        private readonly string _eventString;
+        private readonly IListener _fn;
+        private readonly Emitter _emitter;
+
+        public OnceListener(string eventString, IListener fn, Emitter emitter)
+        {
+            this._eventString = eventString;
+            this._fn = fn;
+            this._emitter = emitter;
+        }
 
         void IListener.Call(params object[] args)
         {
-            throw new NotImplementedException();
+            _emitter.Off(_eventString, this);
+            _fn.Call(args);
         }
     }
 }
--- EngineIoClientDotNet_Tests/ComponentEmitterTests/EmitterTests.cs
+++ EngineIoClientDotNet_Tests/ComponentEmitterTests/EmitterTests.cs
@@ -3,15 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Quobject.EngineIoClientDotNet.ComponentEmitter;
 using Xunit;
 
 namespace Quobject.EngineIoClientDotNet_Tests.ComponentEmitterTests
 {
     public class EmitterTests
     {
-        [Fact]
-        public void Decode()
+        public class TestListener1 : IListener
         {
+            private readonly List<object> _calls;
+
+            public TestListener1(List<object> calls)
+            {
+                this._calls = calls;
+            }                  
+
+            public void Call(params object[] args)
+            {
+                _calls.Add("one");
+                _calls.Add(args[0]);
+            }
+        }
+
+        public class TestListener2 : IListener
+        {
+            private readonly List<object> _calls;
+
+            public TestListener2(List<object> calls)
+            {
+                this._calls = calls;
+            }
+
+            public void Call(params object[] args)
+            {
+                _calls.Add("two");
+                _calls.Add(args[0]);
+            }
+        }
+
+        [Fact]
+        public void On()
+        {            
+            var emitter = new Emitter();
+            var calls = new List<object>();
+
+            var listener1 = new TestListener1(calls);
+            emitter.On("foo", listener1);
+
+            var listener2 = new TestListener2(calls);
+            emitter.On("foo", listener2);
+
+            emitter.Emit("foo", 1);
+            emitter.Emit("bar", 1);
+            emitter.Emit("foo", 2);
+
+            var expected = new Object[] {"one", 1, "two", 1, "one", 2, "two", 2};
+            Assert.Equal(calls.ToArray(), expected);
         }
     }
 }
