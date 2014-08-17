67b62ed4f7ffcaf5b71fbd3bf6e2da79fae51e69
MatthiasL <matthias_ludwig@yahoo.com>
2014-08-17 12:16:56 +1000

    Transport done

--- EngineIoClientDotNet/Client/Transport.cs
+++ EngineIoClientDotNet/Client/Transport.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using Quobject.EngineIoClientDotNet.ComponentEmitter;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using Quobject.EngineIoClientDotNet.Parser;
+using Quobject.EngineIoClientDotNet.Thread;
 
 
 namespace Quobject.EngineIoClientDotNet.Client
@@ -41,7 +44,7 @@ namespace Quobject.EngineIoClientDotNet.Client
         public SSLContext SSLContext;
         protected System.Net.Sockets.Socket Socket;
 
-        protected ReadyStateEnum ReadyState;
+        protected ReadyStateEnum ReadyState = ReadyStateEnum.CLOSED;
 
         public Transport(Options options)
         {
@@ -63,11 +66,86 @@ namespace Quobject.EngineIoClientDotNet.Client
             return this;
         }
 
-        public Transport Open()
+        protected void OnOpen()
         {
-            return null;
+            ReadyState = ReadyStateEnum.OPEN;
+            Writable = true;
+            Emit(EVENT_OPEN);
         }
 
+        protected void OnClose()
+        {
+            ReadyState = ReadyStateEnum.CLOSED;
+            Emit(EVENT_CLOSE);
+        }
+
+
+        protected void OnData(string data)
+        {
+            this.OnPacket(Parser.Parser.DecodePacket(data));
+        }
+
+        protected void OnData(byte[] data)
+        {
+            this.OnPacket(Parser.Parser.DecodePacket(data));
+        }
+
+        protected void OnPacket(Packet packet)
+        {
+            this.Emit(EVENT_PACKET, packet);
+        }
+
+
+        public Transport Open()
+        {
+            EventTasks.Exec(n =>
+            {
+                if (ReadyState == ReadyStateEnum.CLOSED)
+                {
+                    ReadyState = ReadyStateEnum.OPENING;
+                    DoOpen();
+                    //OnOpen();
+                }            
+            });
+            return this;
+        }
+
+        public Transport Close()
+        {
+            EventTasks.Exec(n =>
+            {
+                if (ReadyState == ReadyStateEnum.OPENING || ReadyState == ReadyStateEnum.OPEN)
+                {
+                    DoClose();
+                    OnClose();
+                }
+            });
+            return this;
+        }
+
+        public Transport Send(ImmutableList<Packet> packets )
+        {
+            EventTasks.Exec(n =>
+            {
+                if (ReadyState == ReadyStateEnum.OPEN)
+                {
+                    DoSend(packets);
+                }
+                else
+                {
+                    throw new EngineIOException("Transport not open");
+                }
+            });
+            return this;
+        }
+
+
+
+        protected abstract void DoOpen();
+
+        protected abstract void DoClose();
+
+        protected abstract void DoSend(ImmutableList<Packet> packets);
 
 
         public class Options
