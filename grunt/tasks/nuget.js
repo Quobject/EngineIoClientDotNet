40401de597e3796b8a96e038c025847d9bcc3500
MatthiasL <matthias_ludwig@yahoo.com>
2014-08-28 08:15:27 +1000

    nuget

---
+++ grunt/tasks/nuget.js
@@ -0,0 +1,29 @@
+﻿module.exports = function (grunt) {
+
+  grunt.registerTask('nuget',
+      'get assemplys ', function () {
+    var
+      string = require('string-formatter'),
+      os = grunt.config('os'),
+      config = grunt.config('config'),
+      tasks = [],
+      format_str = os === 'win' ?
+        '{0} restore {1}' :
+        '',
+      nuget_path = os === 'win' ?
+        config.win.nuget : config.linux.nuget,    
+      solution = __dirname + '/../../EngineIoClientDotNet.sln';
+
+
+    grunt.log.writeln('nuget_path = "%s"', nuget_path);
+
+    tasks.push(  string.format(format_str,nuget_path, solution) );
+
+    grunt.log.writeln('tasks = %s', JSON.stringify(tasks));
+    grunt.config('shell.exec.command', tasks.join('&&'));
+    grunt.task.run('shell');
+
+  });
+};
+
+
