acebd11fbcde1a3db2bff46d083a23c18dcd02e8
MatthiasL <matthias_ludwig@yahoo.com>
2014-08-28 08:49:34 +1000

    UsageTest socket close problem

---
+++ EngineIoClientDotNet_Tests/ClientTests/UsageTest.cs
@@ -0,0 +1,38 @@
+﻿using log4net;
+using Quobject.EngineIoClientDotNet.Client;
+using System;
+using System.Collections.Immutable;
+using Xunit;
+
+namespace Quobject.EngineIoClientDotNet_Tests.ClientTests
+{
+    public class UsageTest : Connection
+    {
+
+
+        [Fact]
+        public void Usage1()
+        {
+
+            var socket = new Socket(new Socket.Options { Port = 3000, Hostname = "localhost" });
+            socket.On(Socket.EVENT_OPEN, () =>
+            {
+                socket.Send("hi");
+                //socket.Send("hi", () =>
+                //{
+                //    System.Threading.Thread.Sleep(TimeSpan.FromSeconds(4));
+                //    socket.Close();
+                //});
+            });
+            socket.Open();
+
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(4));
+            socket.Close();
+
+
+
+        }
+
+   
+    }
+}
--- README.md
+++ README.md
@@ -10,6 +10,23 @@ This is the Engine.IO Client Library for C#, which is ported from the [JavaScrip
 
 
 ## Usage
+EngineIoClientDotNet has a similar api to those of the JS client. You can use `Socket` to connect:
+
+```cs
+var options = new Socket.Options();
+options.Port = 3000;
+options.Hostname = "localhost";
+
+socket = new Socket(options);
+socket.On(Socket.EVENT_OPEN, () =>
+{	
+	socket.Send("hi");
+	socket.Close();
+});
+socket.Open();
+```
+            
+
 
 ## Features
 
