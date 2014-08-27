8068b918a1518f1a4251c4cc815bcc97d2c1f807
MatthiasL <matthias_ludwig@yahoo.com>
2014-08-27 16:01:37 +1000

    grunt continued

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,3 +1,15 @@
+// JavaScript equivalent to printf/string.format
+// http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+// First, checks if it isn't implemented yet.
+if (!String.prototype.format) {
+  String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+      return typeof args[number] !== 'undefined' ? args[number] : match
+      ;
+    });
+  };
+}
 
 module.exports = function (grunt) {
   var
---
+++ grunt/tasks/build-client.js
@@ -0,0 +1,31 @@
+﻿module.exports = function (grunt) {
+
+  grunt.registerTask('buildClient',
+      'build cs modules', function () {
+    var
+      os = grunt.config('os'),
+      tasks = [],
+      cs_sln = __dirname + '/../../EngineIoClientDotNet/EngineIoClientDotNet.csproj',
+      build_format_str;
+
+
+
+    if (os === 'win') {
+      build_format_str = 'C:/WINDOWS/System32/WindowsPowerShell/v1.0/powershell.exe start-process -NoNewWindow ' +
+            '-FilePath C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe ' +
+            '-ArgumentList \' {0}  /p:Configuration=Release \' ';
+    } else {
+      build_format_str = "xbuild {0} /p:Configuration=Release";
+    }
+
+    grunt.log.writeln('cs_sln = "%s"', cs_sln);
+    tasks.push(build_format_str.format(cs_sln));
+
+    grunt.log.writeln('tasks = %s', JSON.stringify(tasks));
+    //grunt.config('shell.exec.command', tasks.join('&&'));
+    //grunt.task.run('shell');
+
+  });
+};
+
+
