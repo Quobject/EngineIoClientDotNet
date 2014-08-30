7930409952214d76bc6b18b1f4cec286b4bf3c64
MatthiasL <matthias_ludwig@yahoo.com>
2014-08-31 07:50:37 +1000

    fixed tests

--- grunt/tasks/build-test.js
+++ grunt/tasks/build-test.js
@@ -8,7 +8,7 @@
       config = grunt.config('config'),
       tasks = [],
       csproj = os === 'win' ? __dirname + '/../../EngineIoClientDotNet_Tests/EngineIoClientDotNet_Tests.csproj':
-        __dirname + '/../../EngineIoClientDotNet_Test/EngineIoClientDotNet_Tests_Mono.csproj',
+        __dirname + '/../../EngineIoClientDotNet_Tests/EngineIoClientDotNet_Tests_Mono.csproj',
       build,
       clean;
 
