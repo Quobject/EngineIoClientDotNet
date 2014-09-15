1c32ea4b64b3683c654d0e62dd132e0995fcce95
MatthiasL <matthias_ludwig@yahoo.com>
2014-09-16 07:19:53 +1000

    wpa started

--- Src/EngineIoClientDotNet.Tests.mono/ClientTests/BinaryPollingTest.cs
+++ Src/EngineIoClientDotNet.Tests.mono/ClientTests/BinaryPollingTest.cs
@@ -18,65 +18,65 @@ namespace Quobject.EngineIoClientDotNet_Tests.ClientTests
     public class BinaryPollingTest : Connection
     {
 
-        [Fact]
-        public void PingTest()
-        {
+        //[Fact]
+        //public void PingTest()
+        //{
 
-            var log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod());
-            log.Info("Start");
+        //    var log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod());
+        //    log.Info("Start");
 
-            var binaryData = new byte[5];
-            for (int i = 0; i < binaryData.Length; i++)
-            {
-                binaryData[i] = (byte)i;
-            }
+        //    var binaryData = new byte[5];
+        //    for (int i = 0; i < binaryData.Length; i++)
+        //    {
+        //        binaryData[i] = (byte)i;
+        //    }
 
-            var events = new ConcurrentQueue<object>();
+        //    var events = new ConcurrentQueue<object>();
 
 
-            var options = CreateOptions();
-            options.Transports = ImmutableList.Create<string>(Polling.NAME);
+        //    var options = CreateOptions();
+        //    options.Transports = ImmutableList.Create<string>(Polling.NAME);
 
-            var socket = new Socket(options);
+        //    var socket = new Socket(options);
 
-            socket.On(Socket.EVENT_OPEN, () =>
-            {
+        //    socket.On(Socket.EVENT_OPEN, () =>
+        //    {
 
-                log.Info("EVENT_OPEN");
+        //        log.Info("EVENT_OPEN");
 
-                socket.Send(binaryData);
-                socket.Send("cash money €€€");
-            });
+        //        socket.Send(binaryData);
+        //        socket.Send("cash money €€€");
+        //    });
 
-            socket.On(Socket.EVENT_MESSAGE, (d) =>
-            {
+        //    socket.On(Socket.EVENT_MESSAGE, (d) =>
+        //    {
 
-                var data = d as string;
-                log.Info(string.Format("EVENT_MESSAGE data ={0} d = {1} ", data, d));
+        //        var data = d as string;
+        //        log.Info(string.Format("EVENT_MESSAGE data ={0} d = {1} ", data, d));
 
-                if (data == "hi")
-                {
-                    return;
-                }
-                events.Enqueue(d);
-                //socket.Close();
-            });
+        //        if (data == "hi")
+        //        {
+        //            return;
+        //        }
+        //        events.Enqueue(d);
+        //        //socket.Close();
+        //    });
 
-            socket.Open();
-            Task.Delay(20000).Wait();
-            socket.Close();
-            log.Info("ReceiveBinaryData end");
+        //    socket.Open();
+        //    Task.Delay(20000).Wait();
+        //    socket.Close();
+        //    log.Info("ReceiveBinaryData end");
 
-            var binaryData2 = new byte[5];
-            for (int i = 0; i < binaryData2.Length; i++)
-            {
-                binaryData2[i] = (byte)(i + 1);
-            }
+        //    var binaryData2 = new byte[5];
+        //    for (int i = 0; i < binaryData2.Length; i++)
+        //    {
+        //        binaryData2[i] = (byte)(i + 1);
+        //    }
 
-            object result;
-            events.TryDequeue(out result);
-            Assert.Equal("1", "1");
-        }
+        //    object result;
+        //    events.TryDequeue(out result);
+        //    Assert.Equal("1", "1");
+        //}
 
         private ManualResetEvent _manualResetEvent = null;
 
