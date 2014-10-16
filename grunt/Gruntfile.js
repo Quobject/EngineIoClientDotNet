7fcfccca11d92238008291b924ede141419562fa
MatthiasL <matthias_ludwig@yahoo.com>
2014-10-16 12:36:10 +1100

    0.9.11 release

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -10,8 +10,8 @@ module.exports = function (grunt) {
       { "Name": "EngineIoClientDotNet.net45", "NuGetDir": "net45" },
       { "Name": "EngineIoClientDotNet.windowsphone8", "NuGetDir": "windowsphone8" },
       { "Name": "EngineIoClientDotNet.netcore45", "NuGetDir": "netcore45" },
-      { "Name": "EngineIoClientDotNet.portable-wpa81+wp81", "NuGetDir": "portable-wpa81+wp81", copyOnly:true },
-    { "Name": "EngineIoClientDotNet.portable-win81+wpa81", "NuGetDir": "portable-win81+wpa81" }
+      { "Name": "EngineIoClientDotNet.portable-wpa81+wp81", "NuGetDir": "portable-wpa81+wp81" },
+    { "Name": "EngineIoClientDotNet.portable-win81+wpa81", "NuGetDir": "portable-win81+wpa81", copyOnly: true }
   ];
 
   grunt.log.writeln(util.inspect(config));
